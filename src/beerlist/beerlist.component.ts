9ff428591781f89ce21cf00c29a6a5c5b3eaa73a
Joel Binnquist <joel.binnquist@gmail.com>
2016-05-08 08:20:41 +0200

    Putsa lite mer.

--- src/beerlist/beerlist.component.ts
+++ src/beerlist/beerlist.component.ts
@@ -9,11 +9,11 @@ import {BeerService} from "../beer/beer.service";
   directives: [BeerDetailsComponent],
   template: `
 <h2>Öllistan</h2>
-<ol>
-  <li *ngFor="let beer of getBeers()"><span class="badge">{{beer.points}}</span>
-    <a (click)="select(beer)">{{beer.name}}</a> <a class="danger" href="#" (click)="remove(beer)">[ta bort]</a>
-  </li>
-</ol>
+<ul>
+  <div *ngFor="let beer of getBeers()"><span class="badge">{{beer.points}}</span>
+    <a (click)="select(beer)">{{beer.name}} ({{beer.id}})</a> <a class="danger remove" href="#" (click)="remove(beer)">[ta bort]</a>
+  </div>
+</ul>
 <button type="button" (click)="addBeer()">Lägg till ölsort</button>
 `
 })
