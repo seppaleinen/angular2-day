8d03cd5bc022c5507d52b2fe773fff5ffcf6fc34
David Eriksson <davideriksson@swedenmail.com>
2016-05-13 10:37:08 +0200

    awesome

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,16 +4,49 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <h1>Min Angular2-komponent</h1>
-  <p>{{name}}</p>
-  <input type="text" [(ngModel)]="name">
-  <p [class.danger]="name && name.length > 10">{{name}}</p>
-  <button type="button" (click)="name=undefined">Rensa</button>
+  <p>Sekunder sedan sidan laddades: {{seconds}}s</p>
+
+  <hr>
+
+    <h2>Ölsort: {{beer}}</h2>
+    <dl>
+        <dt>Beskrivning</dt>
+        <dd>{{description}}</dd>
+    </dl>
+
+    <hr>
+
+    Ange ölsort: <input type="text" [(ngModel)]="beer"><br><br>
+    Karaktär: <input type="text" [(ngModel)]="description">
+
+    <br/>
+    <button type="button" (click)="clear()" [disabled]="isEmpty()">Rensa</button>
   `
 })
 export class AppComponent {
+    seconds: number = -1;
+
+    beer: string;
+    description: string;
+
     name:string='CAG Contactor';
 
     constructor() {
         console.log("LOGGYLOG");
+        this.nextTimeout();
     }
+
+    nextTimeout() {
+        this.seconds++;
+        setTimeout(() => this.nextTimeout(), 1000);
+    }
+
+    clear() {
+        this.beer = undefined;
+        this.description = undefined;
+    }
+
+    isEmpty() {
+        return (this.beer === undefined || this.beer.length === 0) &&
+            (this.description === undefined || this.description.length === 0)
 }
