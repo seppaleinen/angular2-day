c889b1556d0e3be8fdcabe5197b4487e6f2e3d02
David Eriksson <davideriksson@swedenmail.com>
2016-05-13 10:20:53 +0200

    asd

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Min Angular2-komponent</h1>'
+  template: `
+  <h1>Min Angular2-komponent</h1>
+  <p>{{name}}</p>
+  <input type="text" [(ngModel)]="name">
+  <p [class.danger]="name && name.length > 10">{{name}}</p>
+  <button type="button" (click)="name=undefined">Rensa</button>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+    name:string='CAG Contactor';
+
+    constructor() {
+        console.log("LOGGYLOG");
+    }
+}
